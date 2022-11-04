1d0e63af68a07e0add1d1f21a313a6b0d4d9c63a
Jorden A <69736105+anfins@users.noreply.github.com>
2022-11-04 14:46:20 -0400

    Update vertex.cpp

--- vertex.cpp
+++ vertex.cpp
@@ -4,11 +4,30 @@ template <typename D, typename K>
 class Vertex{
 
     public:
+        Vertex * adjList;
         
-        Vertex(K k, D d, vector <K> e){
+        Vertex(K k, D d){
             key = k;
             data = d;
-            edges = e;
+           
+        }
+
+        void setEdges(vector <K> edges){
+            adjList = new Vertex[edges.length()]
+            
+        }
+        
+
+
+        void setColor(int col){
+            color = col;
+        }
+        D getdata(){ //getter for the vertex's data
+            return data;
+
+        }
+        K getkey(){ //getter for the vertex's key
+            return key;
         }
 
         
@@ -18,6 +37,7 @@ class Vertex{
 
 
     private:
+
         int color;// 0 white, 1 grey, 2 black
         Vertex <D,K> predecessor; //check back later
         K key;
